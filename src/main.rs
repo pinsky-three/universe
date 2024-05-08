ac90f4706fd5601f73c814a3cc28bed2b70e7618
Bregy Malpartida <bregy@minsky.cc>
2024-05-08 12:21:28 -0500

    feat: add itertools and rayon dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use lenia_ca::{kernels, lenias::StandardLenia, Kernel, Simulator};
+// use std::sync::{Arc, Mutex};
+
+use itertools::Itertools;
+use lenia_ca::{kernels, lenias::StandardLenia, Simulator};
 use macroquad::prelude::*;
 use ndarray::ArrayD;
 use ndarray_rand::{rand_distr::Uniform, RandomExt};
+// use rayon::prelude::*;
 
-const WIDTH: i32 = 800;
-const HEIGHT: i32 = 800;
+const WIDTH: i32 = 12800;
+const HEIGHT: i32 = 2200;
 
-const CELL_SIZE: usize = 4;
+const CELL_SIZE: usize = 8;
 
 fn window_conf() -> Conf {
     Conf {
@@ -34,6 +38,9 @@ async fn main() {
     );
 
     let mut image = Image::gen_image_color(window_width as u16, window_height as u16, WHITE);
+
+    // Image::gen_image_color(cell_size as u16, cell_size as u16, WHITE);
+
     let texture = Texture2D::from_image(&image);
 
     let starting_pattern = ArrayD::random(
@@ -49,10 +56,25 @@ async fn main() {
     loop {
         simulator.iterate();
 
+        // for x in 0..grid_width {
+        //     for y in 0..grid_height {
+        //         let val = (channel[[x, y]] * 255.0) as u8;
+        //         let color = Color::from_rgba(val, val, val, 255);
+
+        //         for px in x * cell_size..(x + 1) * cell_size {
+        //             for py in y * cell_size..(y + 1) * cell_size {
+        //                 image.set_pixel(px as u32, py as u32, color);
+        //             }
+        //         }
+        //     }
+        // }
+
         let channel = simulator.get_channel_as_ref(0);
 
-        for x in 0..grid_width {
-            for y in 0..grid_height {
+        (0..grid_width)
+            .cartesian_product(0..grid_height)
+            // .par_bridge()
+            .for_each(|(x, y)| {
                 let val = (channel[[x, y]] * 255.0) as u8;
                 let color = Color::from_rgba(val, val, val, 255);
 
@@ -61,15 +83,18 @@ async fn main() {
                         image.set_pixel(px as u32, py as u32, color);
                     }
                 }
-            }
-        }
+            });
 
         texture.update(&image);
         draw_texture(&texture, 0., 0., WHITE);
+
+        let fps_text = format!("FPS: {}", get_fps());
+        draw_text(&fps_text, 10., 30., 28., GREEN);
+
         next_frame().await;
 
         simulator.set_kernel(
-            kernels::gaussian_donut_2d(rand::gen_range(2, 20), 1.0 / 6.7),
+            kernels::gaussian_donut_2d(rand::gen_range(8, 20), 1.0 / 6.7),
             0,
         );
     }
